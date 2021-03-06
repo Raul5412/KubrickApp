aab9e92c6bb0c39d63ee69c9ed6a2abfbde785d7
David Bond <descarcega@gmail.com>
2021-03-05 21:14:09 -0600

    Proyecto Con NavComponent

--- app/src/main/java/edu/itesm/stanley/KubrickFragment.kt
+++ app/src/main/java/edu/itesm/stanley/KubrickFragment.kt
@@ -1,10 +1,13 @@
 package edu.itesm.stanley
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import kotlinx.android.synthetic.main.fragment_kubrick.*
 
 // TODO: Rename parameter arguments, choose names that match
@@ -34,7 +37,15 @@ class KubrickFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-
+        movies.setOnClickListener {
+            Log.i("kubrick", "Nav Component" )
+            view?.findNavController()?.navigate(R.id.action_kubrickFragment_to_moviesFragment)
+            //Navigation.createNavigateOnClickListener(  R.id.action_kubrickFragment_to_moviesFragment  )
+        }
+        actors.setOnClickListener {
+            view?.findNavController()?.navigate(R.id.action_kubrickFragment_to_actorskFragment)
+        //Navigation.createNavigateOnClickListener(R.id.action_kubrickFragment_to_actorskFragment,null )
+        }
     }
 
 
--- app/src/main/java/edu/itesm/stanley/Movie.kt
+++ app/src/main/java/edu/itesm/stanley/Movie.kt
@@ -2,7 +2,10 @@ package edu.itesm.stanley
 
 import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
-import java.io.Serializable
 
 
-data class Movie(val picture:Int, val titulo:String, val anio:String, val plot: String)
+@Parcelize
+data class Movie(val picture:Int,
+                 val titulo:String,
+                 val anio:String,
+                 val plot: String) : Parcelable
